5b73375e155dbcda4037b42e795c9dd924ad5238
Limas Jaya Akeh <limas.akeh@binus.edu>
2022-10-25 14:48:33 +0700

    feat: added C# with password and secretKey

---
+++ TestApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var secretKey = "ThisIsMyKey";
+            var password = "TestPassword";
+
+            Console.WriteLine(secretKey);
+        }
+    }
+}
