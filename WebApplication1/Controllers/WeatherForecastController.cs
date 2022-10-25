862f98eaace62c86ad13a4ab8158780e3ad852f9
Limas Jaya Akeh <limas.akeh@binus.edu>
2022-10-25 15:05:51 +0700

    feat: add some vulnerabilities

--- WebApplication1/Controllers/WeatherForecastController.cs
+++ WebApplication1/Controllers/WeatherForecastController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
 
 namespace WebApplication1.Controllers
 {
@@ -23,6 +24,37 @@ namespace WebApplication1.Controllers
         {
             var key = "RNG";
             var password = "TestPassword";
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
+        [HttpGet(Name = "QueryToDatabase")]
+        public IEnumerable<WeatherForecast> GetFromDatabase([FromBody] string query)
+        {
+            var key = "RNG"; // hardcode key
+            var password = "TestPassword"; // hardcode password
+
+            string connectionString = "Server=.\\localhost;Database=TEST_DB;User Id=admin;Password=" + password + ";";
+            SqlConnection connection = new SqlConnection(connectionString);
+
+            SqlCommand command = new SqlCommand(query, connection);
+
+            connection.Open(); // Never closed connection :)
+
+            SqlDataReader reader = command.ExecuteReader();
+
+            while(reader.Read())
+            {
+                Console.WriteLine(String.Format("{0}",
+            reader["id"]));
+            }
+
+
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Date = DateTime.Now.AddDays(index),
